5022448f197f61c34ebb62c59135e3e4f3bc3e05
jak7b <olackovic89@gmail.com>
2023-10-22 18:26:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Include any interactive elements for your website here.
+// This is a simple placeholder script for demonstration purposes.
+document.querySelector("h1").addEventListener("click", function() {
+    alert("You clicked the header!");
+});
